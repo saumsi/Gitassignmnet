64483db56a8cca198c3c0b2c25afaccc7b7e9258
saumsi <saumyasingh.2201@gmail.com>
2020-06-21 17:46:05 +0530

    feat: Addition and Multiplication

--- calculator.java
+++ calculator.java
@@ -1,1 +1,21 @@
-"Hi I am empty calculator"
+import java.util.*;
+
+public class calculator
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Multiplication of "+a+" & "+b+" is "+multiplication(a,b));
+		System.out.println("Addition of "+a+" & "+b+" is "+addition(a,b));
+	}
+	public static int multiplication(int a, int b)
+	{
+		return a*b;
+	}
+	public static int addition(int a, int b)
+	{
+		return a+b;
+	}
+}
