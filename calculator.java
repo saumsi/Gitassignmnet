a89aa5acef9854d3dcdb61e22daeba3405405256
saumsi <saumyasingh.2201@gmail.com>
2020-06-23 13:39:30 +0530

    commiting calculator.java and file1

--- calculator.java
+++ calculator.java
@@ -1,1 +1,30 @@
-"Hi I am empty calculator"
+
+
+import java.util.*;
+public class calculator
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a= sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Subtraction of a and b is "+subtraction(a,b));
+		System.out.println("Division of a and b is "+division(a,b));
+	}
+	public static int subtraction(int a, int b)
+	{
+		return a-b;
+	}
+	public static int division(int a , int b)
+	{
+		return a/b;
+  }
+	public static int multiplication(int a, int b)
+	{
+		return a*b;
+	}
+	public static int addition(int a, int b)
+	{
+		return a+b;
+
+	}
+}
